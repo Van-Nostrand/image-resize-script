8133bc8e8029a36e63e8243672b05ccce9227540
Van-Nostrand <dmdoull43@gmail.com>
2021-03-09 12:19:46 -0800

    updated script to parse long pathnames, filenames with multiple periods

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const sharp = require('sharp');
 const args = process.argv.slice(2);
-const splitstring = args[0].split('.');
+const firstsplit = args[0].split('/');
+const splitstring = firstsplit[firstsplit.length - 1].split('.');
+const extension = splitstring[splitstring.length - 1];
+splitstring.pop();
+const filename = splitstring.join('-');
+const imgsizename = `@${args[1]}`;
+const fullFileName = filename + imgsizename + "." + extension;
 
 sharp(args[0])
   .resize({
     height: parseInt(args[1])
   })
-  .toFile(`${splitstring[0]}-thumbnail.${splitstring[1]}`)
+  .toFile(fullFileName)
   .then(info => console.log(info))
   .catch(err => console.log(err));
