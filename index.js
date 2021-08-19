c881c180946ad0263ae0bea1ce26c2cd46eeabad
Van-Nostrand <dmdoull43@gmail.com>
2021-08-19 11:16:50 -0700

    added comments, error checking for the optional argument

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/*
+this takes up to two arguments
+the first is the image to convert
+the second, if provided, is the height of the desired thumbnail
+if no second argument is provided then this returns 4 files at assumed heights
+
+note for self:
+process.argv is the array of arguments provided when running a node script
+this script is started thusly: node index.js <filename> <number|optional>
+therefore, of process.argv: 
+the first will be the absolute path of the node binary
+the second will be the absolute path of this script
+the thid will be the RELATIVE path of the file to act on
+and if a fourth is passed in, then it's just a number
+*/
 const sharp = require('sharp');
 const args = process.argv.slice(2);
 const firstsplit = args[0].split('/');
@@ -16,12 +31,18 @@ const makeFile = (fullpath, filename, height, extension) => {
     .catch(err => console.log(err));
 }
 
+// if no size argument is passed, assume files at 75, 150, 320, and 768 px high
 if(!args[1]){
   makeFile(args[0], filename, 75, extension);
   makeFile(args[0], filename, 150, extension);
   makeFile(args[0], filename, 320, extension);
   makeFile(args[0], filename, 768, extension);
 }
-else {
+// if size argument is passed, make one file at that size
+else if (args[1] && !isNaN(parseInt(args[1]))) {
   makeFile(args[0], filename, args[1], extension);
+}
+// if the user tried to pass anything other than a number
+else if (isNaN(parseInt(args[1]))) {
+  console.log("you didn't pass a valid number in, try again");
 }
